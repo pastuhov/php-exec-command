b9a384cae9a9cf7540d6c3d71c29a329d7299595
kirill <kirill@pastukhov.su>
2015-06-19 19:25:44 +0500

    add code and tests

--- src/Command.php
+++ src/Command.php
@@ -1,2 +1,77 @@
 <?php
+namespace pastuhov\Command;
 
+/**
+ * Command
+ * @package pastuhov\Command
+ */
+final class Command
+{
+    private function __construct() {}
+
+    /**
+     * Execute command with params.
+     * @param string $commandLine
+     * @param array $params
+     * @return bool|string
+     * @throws \Exception
+     */
+    public static function exec($commandLine, array $params = array())
+    {
+        if (empty($commandLine)) {
+            throw new \Exception('Command line is empty');
+        }
+
+        $commandLine = self::bindParams($commandLine, $params);
+
+        exec($commandLine, $output, $code);
+
+        if (count($output) === 0) {
+            $output = $code;
+        } else {
+            $output = implode(PHP_EOL, $output);
+        }
+
+        if ($code !== 0) {
+            throw new \Exception($output);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Bind params to command.
+     * @param string $commandLine
+     * @param array $params
+     * @return string
+     */
+    public static function bindParams($commandLine, array $params)
+    {
+
+        if (count($params) > 0) {
+            $wrapper = function ($string) {
+                return '{'.$string.'}';
+            };
+            $converter = function ($var) {
+                if (is_array($var)) {
+                    $var = implode(' ', $var);
+                }
+                return $var;
+            };
+
+            $commandLine = str_replace(
+                array_map(
+                    $wrapper,
+                    array_keys($params)
+                ),
+                array_map(
+                    $converter,
+                    array_values($params)
+                ),
+                $commandLine
+            );
+        }
+
+        return $commandLine;
+    }
+}
--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -1,3 +1,45 @@
 <?php
+namespace pastuhov\Command\Test;
 
+use pastuhov\Command\Command;
 
+/**
+ * Class FileStreamTest
+ * @package pastuhov\Command\Test
+ */
+class FileStreamTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExec()
+    {
+        $output = Command::exec(
+            'echo {phrase}',
+            [
+                'phrase' => [
+                    'hello',
+                    'world'
+                ]
+            ]
+        );
+
+        $this->assertEquals('hello world', $output);
+    }
+
+    public function testExecException()
+    {
+        $this->setExpectedException('Exception');
+
+        $output = Command::exec(
+            'echo111'
+        );
+
+    }
+
+    public function testExecEmptyCommand()
+    {
+        $this->setExpectedException('Exception');
+
+        $output = Command::exec(
+            ''
+        );
+    }
+}
